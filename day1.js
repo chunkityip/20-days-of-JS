8bd72fb44c6c4e2ffc91f7259ec2e88fd09c6b85
chunkityip <alex0917lfo@hotmail.com>
2025-01-11 17:38:23 -0500

    first message

---
+++ day1.js
@@ -0,0 +1,21 @@
+// function double(x) {
+//     return x * 2;
+// }
+
+const value =  double = (x) => {
+    return x * 2;
+};
+
+console.log(value);
+
+const value1 = multiply = (x) => {
+    return x * 2;
+};
+
+const name = greet = (name) => {
+    return "Hello" + name;
+};
+
+isLong = (text) => {
+    return text.length > 5;
+}
